c5bbbf9e9eb1ccf7b6d6b2141229105bf91aa14c
Abiscula <victor.augusto@bcrservicos.com.br>
2022-05-13 15:39:13 -0300

    adicionado provedor ao index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { DataProvider } from '../src/provider/dataProvider';
 
 
 const root = ReactDOM.createRoot(
@@ -8,6 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>
 );
