752a637b3ecb1374165b030b1082eb89b750354a
Abiscula <victor.augusto@bcrservicos.com.br>
2022-05-16 07:29:40 -0300

    criado componente para renderização da tabela

--- src/components/DataSet/Table/index.tsx
+++ src/components/DataSet/Table/index.tsx
@@ -1,6 +1,35 @@
+import { useDataContext } from "../../../provider/dataProvider"
 
 export function Table() {
+
+    const { table } = useDataContext()
+
     return(
-        <h1>Table</h1>
+        <div>
+            <h1>Table</h1>
+            <table>
+                {table.map((item: Array<string> | any, index: number) => {
+                    if(index === 0) {
+                    return (
+                        <tr>
+                            <th>{item.A}</th>
+                            <th>{item.B}</th>
+                            <th>{item.C}</th>
+                            <th>{item.D}</th>
+                        </tr>
+                        )
+                    } else {
+                        return (
+                            <tr>
+                                <td>{item.A}</td>
+                                <td>{item.B}</td>
+                                <td>{item.C}</td>
+                                <td>{item.D}</td>
+                            </tr>
+                        )
+                    }
+                })}
+            </table>
+        </div>
     )
 }
