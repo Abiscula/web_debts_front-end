50cdeb2a5355328d806904155a8064c9ca53642e
Abiscula <victor.augusto@bcrservicos.com.br>
2022-05-16 09:57:56 -0300

    criado estilização global da aplicação

---
+++ src/style.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    body {
+        background-color: var(--background);
+        font-family: 'Poppins', sans-serif;
+        font-weight: var(--font-normal);
+    }
+
+`
