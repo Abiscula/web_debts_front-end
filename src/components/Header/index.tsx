2490f2bb129a1838a6d3a970bd124a006636db48
Victor <victorabiscula@live.com>
2022-05-21 16:08:16 -0300

    removido array de meses e adicionado botão para baixar template

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,30 +1,33 @@
 import { useState } from "react";
-import { Container, InputSpan } from "./style";
 import axios from "axios";
+
+import { months } from "../../utils/months";
+import { Container, InputSpan } from "./style";
 import { useDataContext } from "../../provider/dataProvider";
+import { createXLSX } from "../../utils/downloadTemplate";
 
 export function Header() {
 
     const [uploadFile, setUploadFile] = useState<File | null>()
     const [selectedMonth, setSelectedMonth] = useState<string>('')
     const { setTable } = useDataContext()
-    const months = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 
     async function sendFile() {
         console.log(selectedMonth)
         if (uploadFile && selectedMonth) {
-            try{
+            try {
                 const resp = await axios.request({
                     method: 'post',
                     baseURL: 'http://localhost:3333/table',
                     headers: { 'Content-Type': 'multipart/form-data' },
-                    data: { 
+                    data: {
                         table: uploadFile,
                         month: selectedMonth
                     }
                 })
                 setTable(resp.data)
-            } catch(error) {
+            } catch (error) {
                 console.log(error)
             }
         }
@@ -36,6 +39,7 @@ export function Header() {
                 <h1>Web&<span>debts</span></h1>
             </div>
             <div>
+
                 <InputSpan uploadFile={uploadFile}>
                     <label htmlFor="upload">{uploadFile ? uploadFile.name : 'Select CSV'}</label>
                     <input
@@ -45,14 +49,16 @@ export function Header() {
                         onChange={(e) => setUploadFile(e.target.files && e.target.files[0])}
                     />
                 </InputSpan>
-
                 <select onChange={(e) => setSelectedMonth(e.target.value)}>
                     {months.map((month: string) => (
                         <option>{month}</option>
                     ))}
                 </select>
+
+
                 <button onClick={sendFile} disabled={uploadFile === null}> Import table</button>
             </div>
+            <button className="btn-template" onClick={createXLSX}>Baixar template</button>
         </Container>
     )
 }
