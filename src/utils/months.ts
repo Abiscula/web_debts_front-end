b155e6e83945be08adc7d1d9038490c99a1f6ed4
Victor <victorabiscula@live.com>
2022-05-21 16:08:35 -0300

    criado constante com todos meses do ano

---
+++ src/utils/months.ts
@@ -0,0 +1,3 @@
+export const months =   ['', 'January', 'February', 'March', 'April', 
+                        'May', 'June', 'July', 'August', 'September', 'October', 
+                        'November', 'December']
