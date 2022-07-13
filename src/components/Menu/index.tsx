48a800407bc916294c68aaddc8bb531572ace334
Victor Abiscula <victorabiscula@live.com>
2022-07-12 21:58:31 -0300

    adicionado validação nas páginas de estatisticas e graficos

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,18 @@
 import { TrendUp, ChartPieSlice, MicrosoftExcelLogo } from "phosphor-react";
 import { Link } from "react-router-dom";
+import { useDataContext } from "../../provider/dataProvider";
 import { Container } from "./style";
 
 export function Menu() {
+
+    const { table } = useDataContext()
+
     return (
         <Container>
             <nav>
                 <Link to="/" className="link"><MicrosoftExcelLogo size={32} />Table</Link>
-                <Link to="/statistic" className="link"><TrendUp size={32} />Statistic</Link>
-                <Link to="/graph" className="link"><ChartPieSlice size={32} />Charts</Link>
+                <Link to={table.length === 0 ? '' : "/statistic"} className="link"><TrendUp size={32} />Statistic</Link>
+                <Link to={table.length === 0 ? '' : "/graph"} className="link"><ChartPieSlice size={32} />Charts</Link>
             </nav>
         </Container>
     )
