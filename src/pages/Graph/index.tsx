46892bdffb264e9cf5fd2896fcc134452524c653
Victor Abiscula <victorabiscula@live.com>
2022-07-12 21:59:07 -0300

    criado gráfico de pizza dinâmico para tipos de gasto

--- src/pages/Graph/index.tsx
+++ src/pages/Graph/index.tsx
@@ -1,27 +1,64 @@
+import { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
+import { useDataContext } from "../../provider/dataProvider";
+import { useNavigate } from 'react-router-dom'
 
-const data = [
-  ["Language", "Speakers (in millions)"],
-  ["German", 5.85],
-  ["French", 1.66],
-  ["Italian", 0.316],
-  ["Romansh", 0.0791],
-];
-
-const options = {
-  legend: "none",
-  pieSliceText: "label",
-  title: "Swiss Language Use (100 degree rotation)",
-  backgroundColor: 'none',
-  pieStartAngle: 100,
-};
+interface Props {
+  TYPE: object
+}
 
 export function Graph() {
+
+  const navigate = useNavigate()
+  const { table } = useDataContext()
+  const [data, setData] = useState<Array<string | number>>([])
+
+  useEffect(() => {
+    if(table.length === 0) {
+      navigate('/')
+    }
+  }, [table.length, navigate])
+
+  useEffect(() => {
+    const spentType = getTypes()
+    spentCount(spentType)
+  }, [])
+
+
+  function getTypes() {
+    let newArr: Array<object> = [{}]
+    table.forEach((el: Props) => (
+      newArr.push(el['TYPE'])
+    ))
+    return newArr
+  }
+
+
+  function spentCount(spentType: Array<object>) {
+    let count = spentType.reduce((allTypes: any, types: any) => {
+      if (types in allTypes) {
+        allTypes[types]++;
+      }
+      else {
+        allTypes[types] = 1;
+      }
+      return allTypes;
+    })
+
+    let newArray: Array<string | any> = Object.entries(count)
+    newArray.unshift(["Spent", "Type"])
+    setData(newArray)
+  }
+
+
   return (
     <Chart
       chartType="PieChart"
       data={data}
-      options={options}
+      options={{
+        backgroundColor: 'none',
+        is3D: true,
+      }}
       width={"100%"}
       height={"400px"}
     />
